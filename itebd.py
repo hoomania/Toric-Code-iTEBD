7a3bebbb246c9780420b78ec04d07275c5395888
Hooman <thehoomania@gmail.com>
2023-10-22 15:13:59 +0330

    function for dynamic indices

--- itebd.py
+++ itebd.py
@@ -6,32 +6,42 @@ import numpy as np
 
 class iTEBD:
     # lambda_b, gamma_a, lambda_a, gamma_b, lambda_b
-    MPS_NODE_INDICES = [
-        [3, 0, 1, 2, 3],
-        [1, 2, 3, 0, 1],
-    ]
-    MPS_CONTRACT_LEGS_INDICES = [
-        [-1, 1],
-        [1, 2, 3],
-        [3, 4],
-        [4, 5, 6],
-        [6, -2],
-        [2, 5, -3, -4],
-    ]
-    MPS_CONTRACT_FINAL_ORDER = [-1, -3, -4, -2]
-
-    EXPECTATION_MPS_CONTRACT_LEG_INDICES = [
-        [-1, 1],
-        [1, -2, 2],
-        [2, 3],
-        [3, -3, 4],
-        [4, -4],
-    ]
-    EXPECTATION_CONTRACT_LEGS_INDICES = [
-        [1, 2, 3, 4],
-        [2, 3, 5, 6],
-        [1, 5, 6, 4]
-    ]
+    # MPS_NODE_INDICES = [
+    #     [3, 0, 1, 2, 3],
+    #     [1, 2, 3, 0, 1],
+    # ]
+    # # MPS_CONTRACT_LEGS_INDICES = [
+    # #     [-1, 1],
+    # #     [1, 2, 3],
+    # #     [3, 4],
+    # #     [4, 5, 6],
+    # #     [6, -2],
+    # #     [2, 5, -3, -4],
+    # # ]
+    #
+    # MPS_CONTRACT_LEGS_INDICES = [
+    #     [-1, 1],
+    #     [1, 2, 3],
+    #     [3, 4],
+    #     [4, 5, 6],
+    #     [6, -4],
+    #     [2, 5, -2, -3],
+    # ]
+    #
+    # # MPS_CONTRACT_FINAL_ORDER = [-1, -3, -4, -2]
+    #
+    # EXPECTATION_MPS_CONTRACT_LEG_INDICES = [
+    #     [-1, 1],
+    #     [1, -2, 2],
+    #     [2, 3],
+    #     [3, -3, 4],
+    #     [4, -4],
+    # ]
+    # EXPECTATION_CONTRACT_LEGS_INDICES = [
+    #     [1, 2, 3, 4],
+    #     [2, 3, 5, 6],
+    #     [1, 5, 6, 4]
+    # ]
 
     def __init__(
             self,
@@ -39,15 +49,29 @@ class iTEBD:
             physical_dim: int,
             virtual_dim: int = 2,
             unit_cells: int = 1,
-            hamiltonian_title: str = 'untitled'
     ):
         self.hamil = hamiltonian
-        self.hamil_title = hamiltonian_title
         self.phy_dim = physical_dim
         self.vir_dim = virtual_dim
         self.phy_vir_dim = self.phy_dim * self.vir_dim
-        self.delta = 0  # trotter_delta
         self.unit_cells = unit_cells
+
+        self.delta = 0
+        self.accuracy = 1e-16
+
+        self.mps_nodes = self.initial_mps_nodes()
+
+        self.MPS_NODE_INDICES = self.mps_node_indices()
+        self.EXPECTATION_MPS_CONTRACT_LEG_INDICES = self.expectation_mps_contract_leg_indices()
+        self.EXPECTATION_CONTRACT_LEGS_INDICES = self.expectation_contract_legs_indices()
+        self.MPS_CONTRACT_LEGS_INDICES = [
+            [-1, 1],
+            [1, 2, 3],
+            [3, 4],
+            [4, 5, 6],
+            [6, -4],
+            [2, 5, -2, -3],
+        ]
 
     def suzuki_trotter(
             self,
@@ -89,8 +113,9 @@ class iTEBD:
                     tensor_chain[j] = mps_chain_cell[pointer[j]]
                 tensor_chain[5] = trotter_tensor[steps]
 
-                tensor_contraction = ncon(tensor_chain, self.MPS_CONTRACT_LEGS_INDICES, None,
-                                          self.MPS_CONTRACT_FINAL_ORDER)
+                # tensor_contraction = ncon(tensor_chain, self.MPS_CONTRACT_LEGS_INDICES, None,
+                #                           self.MPS_CONTRACT_FINAL_ORDER)
+                tensor_contraction = ncon(tensor_chain, self.MPS_CONTRACT_LEGS_INDICES)
                 # implode
                 implode = np.reshape(tensor_contraction, [self.phy_vir_dim, self.phy_vir_dim])
 
@@ -124,192 +149,6 @@ class iTEBD:
 
         return mps_chain_cell
 
-    def expectation_energy(
-            self,
-            mps_nodes: np.ndarray
-    ) -> float:
-        tensor_chain = [0 for _ in range(5)]
-        expectation_value = [0 for _ in range(2)]
-
-        direction = ['AB', 'BA']
-        for i in range(2):
-            steps = i % 2
-
-            for j in range(5):
-                tensor_chain[j] = mps_nodes[self.MPS_NODE_INDICES[steps][j]]
-
-            contraction = ncon(tensor_chain, self.EXPECTATION_MPS_CONTRACT_LEG_INDICES)
-            expectation_value[i] = ncon(
-                [contraction, self.hamil[direction[steps]], np.conj(contraction)],
-                self.EXPECTATION_CONTRACT_LEGS_INDICES
-            )
-            norm = ncon(
-                [contraction, np.conj(contraction)],
-                [self.EXPECTATION_CONTRACT_LEGS_INDICES[0], self.EXPECTATION_CONTRACT_LEGS_INDICES[0]]
-            )
-
-            expectation_value[i] /= norm
-
-        return sum(expectation_value) / 2
-
-    def expectation_energy_two_cell(
-            self,
-            mps_nodes: np.ndarray
-    ) -> float:
-        tensor_chain = [0 for _ in range(9)]
-        expectation_value = [0 for _ in range(2)]
-
-        t_MPS_NODE_INDICES = [
-            [7, 0, 1, 2, 3, 4, 5, 6, 7],
-            [1, 2, 3, 4, 5, 6, 7, 0, 1],
-        ]
-
-        t_EXPECTATION_MPS_CONTRACT_LEG_INDICES = [
-            [-1, 1],
-            [1, -2, 2],
-            [2, 3],
-            [3, -3, 4],
-            [4, 5],
-            [5, -4, 6],
-            [6, 7],
-            [7, -5, 8],
-            [8, -6],
-        ]
-
-        t_EXPECTATION_CONTRACT_LEGS_INDICES = [
-            [1, 2, 3, 4, 5, 6],
-            [2, 3, 7, 8],
-            [4, 5, 9, 10],
-            [1, 7, 8, 9, 10, 6],
-        ]
-
-        direction = ['AB', 'BA']
-        for i in range(2):
-            steps = i % 2
-            # print(len(mps_nodes))
-            for j in range(9):
-                tensor_chain[j] = mps_nodes[t_MPS_NODE_INDICES[steps][j]]
-
-            contraction = ncon(tensor_chain, t_EXPECTATION_MPS_CONTRACT_LEG_INDICES)
-            expectation_value[i] = ncon(
-                [contraction, self.hamil[direction[steps]], self.hamil[direction[steps]], np.conj(contraction)],
-                t_EXPECTATION_CONTRACT_LEGS_INDICES
-            )
-            norm = ncon(
-                [contraction, np.conj(contraction)],
-                [t_EXPECTATION_CONTRACT_LEGS_INDICES[0], t_EXPECTATION_CONTRACT_LEGS_INDICES[0]]
-            )
-
-            expectation_value[i] /= norm
-
-        return sum(expectation_value) / 2
-
-    def expectation_arbitrary_operator(
-            self,
-            mps_nodes: np.ndarray,
-            operator: dict
-    ) -> float:
-        tensor_chain = [0 for _ in range(5)]
-        expectation_value = [0 for _ in range(2)]
-
-        direction = ['AB', 'BA']
-        for i in range(2):
-            steps = i % 2
-
-            for j in range(5):
-                tensor_chain[j] = mps_nodes[self.MPS_NODE_INDICES[steps][j]]
-
-            contraction = ncon(tensor_chain, self.EXPECTATION_MPS_CONTRACT_LEG_INDICES)
-            expectation_value[i] = ncon(
-                [contraction, operator[direction[steps]], np.conj(contraction)],
-                self.EXPECTATION_CONTRACT_LEGS_INDICES
-            )
-            norm = ncon(
-                [contraction, np.conj(contraction)],
-                [self.EXPECTATION_CONTRACT_LEGS_INDICES[0], self.EXPECTATION_CONTRACT_LEGS_INDICES[0]]
-            )
-
-            expectation_value[i] /= norm
-
-        return sum(expectation_value) / 2
-
-    def expectation_arbitrary_operator_two_cell(
-            self,
-            mps_nodes: np.ndarray,
-            operator: dict
-    ) -> float:
-        tensor_chain = [0 for _ in range(9)]
-        expectation_value = [0 for _ in range(2)]
-
-        t_MPS_NODE_INDICES = [
-            [7, 0, 1, 2, 3, 4, 5, 6, 7],
-            [1, 2, 3, 4, 5, 6, 7, 0, 1],
-        ]
-
-        t_EXPECTATION_MPS_CONTRACT_LEG_INDICES = [
-            [-1, 1],
-            [1, -2, 2],
-            [2, 3],
-            [3, -3, 4],
-            [4, 5],
-            [5, -4, 6],
-            [6, 7],
-            [7, -5, 8],
-            [8, -6],
-        ]
-
-        t_EXPECTATION_CONTRACT_LEGS_INDICES = [
-            [1, 2, 3, 4, 5, 6],
-            [2, 3, 7, 8],
-            [4, 5, 9, 10],
-            [1, 7, 8, 9, 10, 6],
-        ]
-
-        direction = ['AB', 'BA']
-        for i in range(2):
-            steps = i % 2
-            # print(len(mps_nodes))
-            for j in range(9):
-                tensor_chain[j] = mps_nodes[t_MPS_NODE_INDICES[steps][j]]
-
-            contraction = ncon(tensor_chain, t_EXPECTATION_MPS_CONTRACT_LEG_INDICES)
-            expectation_value[i] = ncon(
-                [contraction, operator[direction[steps]], operator[direction[steps]], np.conj(contraction)],
-                t_EXPECTATION_CONTRACT_LEGS_INDICES
-            )
-            norm = ncon(
-                [contraction, np.conj(contraction)],
-                [t_EXPECTATION_CONTRACT_LEGS_INDICES[0], t_EXPECTATION_CONTRACT_LEGS_INDICES[0]]
-            )
-
-            expectation_value[i] /= norm
-
-        return sum(expectation_value) / 2
-
-    def expectation_single_site(
-            self,
-            mps_nodes: np.ndarray,
-            operator: dict
-    ) -> float:
-        tensor_chain = [0 for _ in range(5)]
-
-        for j in range(5):
-            tensor_chain[j] = mps_nodes[self.MPS_NODE_INDICES[0][j]]
-
-        contraction = ncon(tensor_chain, self.EXPECTATION_MPS_CONTRACT_LEG_INDICES)
-        expectation_value = ncon(
-            [contraction, operator['AB'], np.conj(contraction)],
-            self.EXPECTATION_CONTRACT_LEGS_INDICES
-        )
-        norm = ncon(
-            [contraction, np.conj(contraction)],
-            [self.EXPECTATION_CONTRACT_LEGS_INDICES[0], self.EXPECTATION_CONTRACT_LEGS_INDICES[0]]
-        )
-
-        expectation_value /= norm
-
-        return sum(expectation_value)
-
     def delta_manager(
             self,
             iteration: int,
@@ -321,6 +160,7 @@ class iTEBD:
 
         result = {
             'mps': self.initial_mps_nodes(),
+            # 'mps': self.mps_nodes,
             'dist': np.inf,
             'energy': np.inf,
         }
@@ -333,11 +173,11 @@ class iTEBD:
         print(f'iTEBD is running on physical dim={self.phy_dim} and virtual dim={self.vir_dim}')
         for delta in np.linspace(delta_start, delta_end, domains):
             self.delta = delta
+            self.accuracy = accuracy
             evo_result = self.evolution(
                 result['mps'],
                 self.suzuki_trotter(delta),
-                iter_value,
-                accuracy
+                iter_value
             )
 
             if evo_result['dist'] < result['dist']:
@@ -350,12 +190,67 @@ class iTEBD:
 
         return result['mps']
 
+    def mps_node_indices(self):
+        len_mps = self.unit_cells * 4
+        indices = [
+            [i % len_mps for i in range(len_mps - 1, len_mps * 2)],
+            [i % len_mps for i in range(1, len_mps + 2)],
+        ]
+        return indices
+
+    def expectation_mps_contract_leg_indices(self):
+        indices_list = [[-1, 1]]
+        positive = 1
+        ppo = 0
+        negative = -2
+        for i in range(1, self.unit_cells * 4):
+            if i % 2 == 1:
+                ppo = positive + 1
+                indices_list.append([positive, negative, ppo])
+                positive += 2
+                negative -= 1
+            else:
+                indices_list.append([ppo, positive])
+
+        indices_list.append([ppo, negative])
+        return indices_list
+
+    def expectation_contract_legs_indices(self):
+        domain = (self.unit_cells * 2) + 2
+        first_row = [i + 1 for i in range(domain)]
+        diff = first_row[-1] - first_row[0]
+        last_row = [first_row[i] + diff for i in range(domain)]
+        last_row[0] = first_row[0]
+        last_row[-1] = first_row[-1]
+        indices_list = [first_row]
+        for i in range(self.unit_cells):
+            indices_list.append(
+                [first_row[(2 * i) + 1], first_row[(2 * i) + 2], last_row[(2 * i) + 1], last_row[(2 * i) + 2]])
+
+        indices_list.append(last_row)
+        return indices_list
+
+    def even_odd_index_generator(self) -> np.ndarray:
+        len_mps = self.unit_cells * 4
+        indexes = [i % len_mps for i in range(len_mps - 1, len_mps * 2)]
+        start_index = [0, 2]  # even, odd
+        start_index_mps = [4 * i for i in range(self.unit_cells)]
+
+        index_divider = []
+        for p in range(2):
+            for q in range(self.unit_cells):
+                index = []
+                for j in range(5):
+                    index.append(indexes[(start_index_mps[q] + start_index[p] + j) % len_mps])
+                index_divider.append(index)
+
+        return np.reshape(index_divider, (2, self.unit_cells, 5))
+
     def evolution(
             self,
             mps_chain_cell: np.ndarray,
             trotter_tensor: list,
-            iteration: int,
-            accuracy: float = 1e-8,
+            iteration: int
     ) -> dict:
 
         # >>>> initial parameters
@@ -382,10 +277,7 @@ class iTEBD:
             )
 
             if i % sampling == 0:
-                if self.unit_cells == 2:
-                    xpc_energy = self.expectation_energy_two_cell(mps_chain_cell)
-                else:
-                    xpc_energy = self.expectation_energy(mps_chain_cell)
+                xpc_energy = self.expectation_value(mps_chain_cell, self.hamil)
 
                 expectation_energy_history.append(xpc_energy)
                 expectation_diff[0] = xpc_energy
@@ -403,28 +295,68 @@ class iTEBD:
                         }
 
             if (i + 1) % sampling == 2:
-                if self.unit_cells == 2:
-                    expectation_diff[1] = self.expectation_energy_two_cell(mps_chain_cell)
-                else:
-                    expectation_diff[1] = self.expectation_energy(mps_chain_cell)
+                expectation_diff[1] = self.expectation_value(mps_chain_cell, self.hamil)
 
-                if np.abs(expectation_diff[0] - expectation_diff[1]) < accuracy:
+                if np.abs(expectation_diff[0] - expectation_diff[1]) < self.accuracy:
                     break
 
         return best_result
 
-    def even_odd_index_generator(self) -> np.ndarray:
-        len_mps = self.unit_cells * 4
-        indexes = [i % len_mps for i in range(len_mps - 1, len_mps * 2)]
-        start_index = [0, 2]  # even, odd
-        start_index_mps = [4 * i for i in range(self.unit_cells)]
+    def expectation_value(
+            self,
+            mps_nodes: np.ndarray,
+            operator: dict
+    ) -> float:
+        tensor_chain = [0 for _ in range((self.unit_cells * 4) + 1)]
+        expectation_value = [0 for _ in range(2)]
 
-        index_divider = []
-        for p in range(2):
-            for q in range(self.unit_cells):
-                index = []
-                for j in range(5):
-                    index.append(indexes[(start_index_mps[q] + start_index[p] + j) % len_mps])
-                index_divider.append(index)
+        direction = ['AB', 'BA']
+        for i in range(2):
+            steps = i % 2
 
-        return np.reshape(index_divider, (2, self.unit_cells, 5))
+            for j in range((self.unit_cells * 4) + 1):
+                tensor_chain[j] = mps_nodes[self.MPS_NODE_INDICES[steps][j]]
+
+            contraction = ncon(tensor_chain, self.EXPECTATION_MPS_CONTRACT_LEG_INDICES)
+
+            list_cell_hamil = [contraction]
+            for w in range(self.unit_cells):
+                list_cell_hamil.append(operator[direction[steps]])
+            list_cell_hamil.append(np.conj(contraction))
+
+            expectation_value[i] = ncon(
+                list_cell_hamil,
+                self.EXPECTATION_CONTRACT_LEGS_INDICES
+            )
+            norm = ncon(
+                [contraction, np.conj(contraction)],
+                [self.EXPECTATION_CONTRACT_LEGS_INDICES[0], self.EXPECTATION_CONTRACT_LEGS_INDICES[0]]
+            )
+
+            expectation_value[i] /= norm
+
+        return sum(expectation_value)
+
+    def expectation_single_site(
+            self,
+            mps_nodes: np.ndarray,
+            operator: dict
+    ) -> float:
+        tensor_chain = [0 for _ in range(5)]
+
+        for j in range(5):
+            tensor_chain[j] = mps_nodes[self.MPS_NODE_INDICES[0][j]]
+
+        contraction = ncon(tensor_chain, self.EXPECTATION_MPS_CONTRACT_LEG_INDICES)
+        expectation_value = ncon(
+            [contraction, operator['AB'], np.conj(contraction)],
+            self.EXPECTATION_CONTRACT_LEGS_INDICES
+        )
+        norm = ncon(
+            [contraction, np.conj(contraction)],
+            [self.EXPECTATION_CONTRACT_LEGS_INDICES[0], self.EXPECTATION_CONTRACT_LEGS_INDICES[0]]
+        )
+
+        expectation_value /= norm
+
+        return sum(expectation_value)
